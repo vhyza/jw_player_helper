5c7a89402d8bf6552e718604a4b8b58bbef9c309
Vojtech Hyza <vhyza@vhyza.eu>
2011-05-15 23:14:19 +0200

    [FIX] Fixed gem file loading

--- lib/jw_player_helper.rb
+++ lib/jw_player_helper.rb
@@ -1,4 +1,17 @@
-require 'jw_player_helper/helper'
+require 'active_support/inflector'
+require 'active_support/core_ext'
+require 'yajl/json_gem'
+require 'pathname'
+
+gem_root = Pathname.new(File.expand_path("../../", __FILE__))
+
+require gem_root.join('lib', 'jw_player_helper', 'configuration')
+
+Dir.glob(gem_root.join('lib', 'jw_player_helper', 'embedders', '*rb')).each do |embedder|
+  require embedder
+end
+
+require gem_root.join('lib', 'jw_player_helper', 'helper')
 
 if defined?(Rails)
 
@@ -8,14 +21,15 @@ if defined?(Rails)
   else
     ActionView::Base.send :include, JWPlayerHelper::Helper
 
-    gem_root = File.expand_path("../../", __FILE__)
-
     javascript_file = Rails.root.join("public", "javascripts", "swfobject.js")
-    gem_javascript_file = Pathname.new(File.join(gem_root, "assets", "javascripts", "swfobject.js"))
 
-    if !File.exists?(javascript_file) or javascript_file.size != gem_javascript_file.size
-      puts "Copying swfobject.js to public/jovascripts"
-      FileUtils.cp_r File.join(gem_root, "assets", "javascripts"), Rails.root.join("public")
+    ["swfobject.js", "jwplayer.js"].each do |js|
+      gem_javascript_file = gem_root.join("assets", "javascripts", js)
+
+      if !File.exists?(javascript_file) or javascript_file.size != gem_javascript_file.size
+        puts "Copying #{js} to public/javascripts"
+        FileUtils.cp_r File.join(gem_root, "assets", "javascripts"), Rails.root.join("public")
+      end
     end
 
     swf_file = Rails.root.join("public", "swf", "player.swf")
