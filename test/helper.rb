d09d3208870f54b5b6edca596db4afc2f24ac8a4
Vojtech Hyza <vhyza@vhyza.eu>
2011-05-15 23:13:52 +0200

    [FIX] Fixed test helper

--- test/helper.rb
+++ test/helper.rb
@@ -1,17 +1,23 @@
 require 'rubygems'
 require 'bundler'
 begin
-  Bundler.setup(:default, :development)
+  Bundler.setup(:default, :development, :test)
 rescue Bundler::BundlerError => e
   $stderr.puts e.message
   $stderr.puts "Run `bundle install` to install missing gems"
   exit e.status_code
 end
+
 require 'test/unit'
+require 'shoulda'
+require 'mocha'
+require 'turn' unless ENV["TM_FILEPATH"]
+
+#NOTE: Engine loading
+require 'rails'
+require 'action_view'
 
 ENV['RAILS_ENV'] = 'test'
-require "rails_app/config/environment"
-require "rails/test_help"
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
